0a488922f5ffef07b45316caad9d3f04e9c7f659
Milad <milad.rabiei.01@gmail.com>
2024-11-14 21:11:13 +0000

    created package and scripts

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/root/turtle_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/root/turtle_ws/devel;/root/my_ros/devel;/opt/ros/noetic'.split(';') if '/root/turtle_ws/devel;/root/my_ros/devel;/opt/ros/noetic' != '' else []
